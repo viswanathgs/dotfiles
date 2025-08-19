978f08cdb145b50a33c5a81713d620bc72d869b3
Viswanath Sivakumar <viswanathgs@gmail.com>
2025-08-19 15:42:19 -0400

    Add numpy and pandas to pip install in setup.sh

--- setup.sh
+++ setup.sh
@@ -55,7 +55,7 @@ function install_deps() {
   fi
 
   echo "\n#### Installing pip dependencies ####\n"
-  pip install --upgrade pre-commit tabcompletion cpplint ptpython pdbpp || true
+  pip install --upgrade pre-commit tabcompletion cpplint ptpython pdbpp numpy pandas tabulate || true
 
   echo "\n#### Installing on-my-zsh ####\n"
   # Install oh-my-zsh into current dir. We'll symlink later to homedir.
